6299505baddf7355c4562be665468f59b66a4060
HALENCAIN <>
2022-02-14 17:28:40 +0000

    Explore8 Working

--- Explore8.cpp
+++ Explore8.cpp
@@ -48,6 +48,18 @@ Point FindMidPoint(Line l) {
     return mid;
 }
 
+Point FindLowestPoint(Line l) {
+  Point lowest;
+  if (l.start.y <= l.end.y){
+    lowest.y = l.start.y;
+    lowest.x = l.start.x;
+  } else {
+    lowest.y = l.end.y;
+    lowest.x = l.end.x;
+  }
+  return lowest;
+}
+
 int main() {
     Line a(Point(3,9), Point(12,22));
     cout << "Line a";
@@ -56,6 +68,15 @@ int main() {
     // This is new
     Point midPoint = FindMidPoint(a);
     PrintPoint(midPoint);
+    cout << endl;
+    cout << "Line a's lowest point";
+    Point lowest = FindLowestPoint(a);
+    PrintPoint(lowest);
+    cout << endl;
+    Line b(Point(7,-9), Point(8,-15));
+    cout << "Line b's lowest point";
+    lowest = FindLowestPoint(b);
+    PrintPoint(lowest);
     cout << endl;
     return 0;
 }
